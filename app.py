41c003dc2e012b70ab81c39da01f5f336d0bdd39
Ahmad-Elraqab <65804256+Ahmad-Elraqab@users.noreply.github.com>
2021-07-28 08:38:29 +0300

    data analysis

--- app.py
+++ app.py
@@ -3,22 +3,25 @@ from config import API_KEY, API_SECRET, exchange_pairs
 from binance.streams import ThreadedWebsocketManager
 from client import handle_socket_message
 import pandas as pd
+from chart_analyzer import getData, analyzePoint
 
 twm = ThreadedWebsocketManager(api_key=API_KEY, api_secret=API_SECRET)
 
 twm.start()
 
-# def handle_symbol_price(msg):
-#     print(msg)
+
+getData("4hr")
+
+
+def handle_symbol_price(msg):
+    # print(msg)
+    analyzePoint("4hr", msg['data']['s'], msg['data']['p'])
+
 
 for pair in exchange_pairs:
-    # twm.start_symbol_mark_price_socket(callback=handle_symbol_price, symbol=pair)
-    twm.start_kline_socket(callback=handle_socket_message, symbol=pair)
-    # data = client.get_ticker(params=pair)
-    # print(pair, '\t\t\t', data)
-    # twm.start_options_kline_socket
 
+    twm.start_symbol_mark_price_socket(
+        callback=handle_symbol_price, symbol=pair)
+    twm.start_kline_socket(callback=handle_socket_message, symbol=pair)
 
 twm.join()
-
-
--- chart_analyzer.py
+++ chart_analyzer.py
@@ -1,61 +1,120 @@
-from os import close
 from binance import Client
-from binance.streams import ThreadedWebsocketManager
 import numpy as np
 from config import API_KEY, API_SECRET, exchange_pairs
-
 from binance.client import Client
 import pandas as pd
 import numpy as np
 
 
-class SR:
-    def __init__(self, isBottom, open, close, high, low):
-        self.isBottom = isBottom
-        self.open = open
-        self.close = close
-        self.high = high
-        self.low = low
-
-
 points_list = {}
 support_list = {}
 resistance_list = {}
-levels = []
+
+current_rs = {
+    'support': 0.0,
+    'resistance': 0.0
+}
+
 client = Client(api_key=API_KEY, api_secret=API_SECRET)
 
 tickers = client.get_all_tickers()
 
 
 def isSupport(df, i):
-    support = float(df[i][3]) < float(df[i-1][3]) and float(df[i][3]) < float(df[i+1][3]) \
-        and float(df[i+1][3]) < float(df[i+2][3]) and float(df[i-1][3]) < float(df[i-2][3])
+    try:
+        support = float(df[i][3]) < float(df[i-1][3]) and float(df[i][3]) < float(df[i+1][3]) \
+            and float(df[i+1][3]) < float(df[i+2][3]) and float(df[i-1][3]) < float(df[i-2][3])
 
-    return support
+        return support
+
+    except:
+
+        print("Invalid Index")
 
 
 def isResistance(df, i):
-    resistance = float(df[i][2]) > float(df[i-1][2]) and float(df[i][2]) > float(df[i+1][2]) \
-        and float(df[i+1][2]) > float(df[i+2][2]) and float(df[i-1][2]) > float(df[i-2][2])
 
-    return resistance
+    try:
+        resistance = float(df[i][2]) > float(df[i-1][2]) and float(df[i][2]) > float(df[i+1][2]) \
+            and float(df[i+1][2]) > float(df[i+2][2]) and float(df[i-1][2]) > float(df[i-2][2])
+
+        return resistance
+
+    except:
+
+        print("Invalid Index")
 
 
-def getData(pair):
+def getData(interval):
 
-    klines = client.get_historical_klines(
-        pair, client.KLINE_INTERVAL_1DAY, "1 oct 2020")
+    for pair in exchange_pairs:
+        klines = client.get_historical_klines(
+            pair, Client.KLINE_INTERVAL_4HOUR, "1 oct 2020")
 
-    for i, value in enumerate(klines):
+        points_list[pair] = {}
+        points_list[pair][interval] = []
 
-        if not ((i == 0) or (i == 1) or i == len(klines) or i == len(klines) - 1):
+        for i, value in enumerate(klines):
+
             if isSupport(klines, i):
                 l = klines[i][3]
-                print("this is a support =>\t\t",l)
-    
+                points_list[pair][interval].append(l)
+
             elif isResistance(klines, i):
                 h = klines[i][2]
-                print("this is a resistance =>\t\t",h)
+                points_list[pair][interval].append(h)
 
-    
-getData('BTCUSDT')
+
+def getInterval(interval):
+    if interval == "1D":
+        return Client.KLINE_INTERVAL_1DAY
+    elif interval == "12hr":
+        return Client.KLINE_INTERVAL_12HOUR
+    elif interval == "8hr":
+        return Client.KLINE_INTERVAL_8HOUR
+    elif interval == "6hr":
+        return Client.KLINE_INTERVAL_6HOUR
+    elif interval == "4hr":
+        return Client.KLINE_INTERVAL_4HOUR
+    elif interval == "1hr":
+        return Client.KLINE_INTERVAL_1HOUR
+    elif interval == "30m":
+        return Client.KLINE_INTERVAL_30MINUTE
+    elif interval == "15m":
+        return Client.KLINE_INTERVAL_15MINUTE
+    elif interval == "5m":
+        return Client.KLINE_INTERVAL_5MINUTE
+    elif interval == "3m":
+        return Client.KLINE_INTERVAL_30MINUTE
+    elif interval == "1m":
+        return Client.KLINE_INTERVAL_1MINUTE
+
+
+def analyzePoint(interval, symbol, current_price):
+
+    support_list[symbol] = {}
+    resistance_list[symbol] = {}
+
+    support_list[symbol][interval] = []
+    resistance_list[symbol][interval] = []
+
+    for point in points_list[symbol][interval]:
+
+        if point > current_price:
+
+            resistance_list[symbol][interval].append(float(point))
+
+        else:
+
+            support_list[symbol][interval].append(float(point))
+
+    current_rs['support'] = find_nearest(
+        support_list[symbol][interval], current_price)
+    current_rs['resistance'] = find_nearest(
+        resistance_list[symbol][interval], current_price)
+
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - float(value))).argmin()
+    return array[idx]
